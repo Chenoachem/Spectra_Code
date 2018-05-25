a3b292e4d53cda5fa4d65dd5e7b1df4e2b33924e
Chenoa Tremblay <chenoa.tremblay@postgrad.curtin.edu.au>
2018-05-25 16:36:49 +0800

    Add files via upload

---
+++ SNR_and_Search.py
@@ -0,0 +1,50 @@
+
+
+import astropy
+from astropy.io import fits
+from astropy.modeling import models
+import numpy as np
+import matplotlib
+#get_ipython().magic(u'matplotlib inline')
+from matplotlib import pyplot
+
+
+
+#open the continuum subtracted fits file
+subcube = fits.open('Orion_I_013_Subtracted.fits')
+data = subcube[0].data
+header = subcube[0].header
+
+# we can just delete the stokes axis
+rm_cube = data[:,0:100,:,:]
+
+
+#replace all the zeros with nans before doing statistical calculations
+rm_cube[rm_cube==0] = np.nan
+
+
+
+#calculate the standard deviation but ignoring nans
+rms_cube=np.nanstd(rm_cube, axis=1)
+
+
+#make a signal to noise map
+snr=rm_cube/rms_cube
+
+#save the signal to noise cube to a fits file
+subcube[0].data = np.float32(snr)
+subcube.writeto('Orion_snr.fits', clobber=True)
+subcube[0].data=np.float32(rms_cube)
+subcube.writeto('Orion_rms.fits', clobber=True)
+
+
+
+
+
+
+
+
+
+
+
+
